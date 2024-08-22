9b98f8ce21e48ce958178908c02c84f0c655276b
Phile105 <163855550+Phile105@users.noreply.github.com>
2024-08-22 15:49:47 +0200

    Move this code the new repository

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Weather from "./Weather";
+
+import "./Weather.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
